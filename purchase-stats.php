a0ac45e5efcb8342b3d5f62559f7187280325cb3
Christian Manalansan <cmanalansan@gmail.com>
2013-10-04 17:45:51 -0400

    For 'purchase stats,' added explanations for scoring mode. Added 'watch football' mockup

--- purchase-stats.php
+++ purchase-stats.php
@@ -26,6 +26,55 @@
                 <p>Will your team dominate? If so, blow your entire bankroll!</p>
                 <p>Are you stuck with a bust? Then maybe it's best to save your money for the next round.</p>
             </div>
+            <h2>Scoring Mode:</h2>
+            <select name="scoring_mode" class="scoring-mode">
+                <option value="standard">Standard</option>
+                <option value="aerial">Aerial Attack</option>
+                <option value="ground">Ground Game</option>
+                <option value="stingyd">Stingy D</option>
+                <option value="justwin">Just Win Baby!</option>
+            </select>
+            <div class="scoring description standard selected">
+                Standard scoring includes the following stats for FREE:
+                <ul>
+                    <li>Passing Yards</li>
+                    <li>Rushing Yards</li>
+                    <li>Defensive Points Allowed</li>
+                </ul>
+            </div>
+            <div class="scoring description aerial selected">
+                Aerial Attack scoring:
+                <ul>
+                    <li>Double points for Passing Yards</li>
+                    <li>No points for Rushing Yards</li>
+                    <li>No points for Defensive Points Allowed</li>
+                </ul>
+            </div>
+            <div class="scoring description ground">
+                Ground Game scoring:
+                <ul>
+                    <li>Double points for Rushing Yards</li>
+                    <li>No points for Passing Yards</li>
+                    <li>No points for Defensive Points Allowed</li>
+                </ul>
+            </div>
+            <div class="scoring description stingyd">
+                Stingy D scoring:
+                <ul>
+                    <li>Double points for Defensive Points Allowed</li>
+                    <li>No points for Passing Yards</li>
+                    <li>No points for Rushing Yards</li>
+                </ul>
+            </div>
+            <div class="scoring description stingyd">
+                Just Win Baby scoring, when team wins:
+                <ul>
+                    <li>Double points for Team Win (x4 instead of x2)</li>
+                    <li>Standard scoring for Passing Yards</li>
+                    <li>Standard scoring for Rushing Yards</li>
+                </ul>
+            </div>
+            <div class="spacer"></div>
             <h2 class="float-left">Stats To Purchase:</h2>
             <div class="remaining float-right">$74 remaining</div>
             <div class="clear"></div>
@@ -61,7 +110,6 @@
                         <td class="stat-name">Team Win?</td>
                         <td class="amount"><input name="team_win_amount" class="form-text" value="44" /></td>
                     </tr>
-
                 </table>
             </div>
             <input type="submit" value="Save" class="form-button" />
---
+++ watch-football.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Money Maker Football - Watch Football!</title>
+    <!-- TODO: remove 'http:' from src when this is eventually hosted -->
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+    <script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js'></script>
+    <!-- CSS Includes -->
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+    <!-- Google Web Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Chango|Oxygen' rel='stylesheet' type='text/css'>
+</head>
+<body class="watch-football-page">
+    <div class="main-container">
+        <form method="post" action="login.php">
+            <h1>Step 3: Watch Football!</h1>
+            <div class="compare-scoring-methods">
+                <table>
+                    <tr class="even">
+                        <th class="stat label">Your Team: Seahwaks</th>
+                        <th class="actual">Actual</th>
+                        <th class="standard">STD</th>
+                        <th class="aerial">AIR</th>
+                        <th class="ground">RUN</th>
+                        <th class="stingyd">DEF</th>
+                        <th class="justwin hidden">WIN</th>
+                        <th class="yours">WIN</th>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Passing Yards</td>
+                        <td class="actual">331</td>
+                        <td class="standard">$66</td>
+                        <td class="aerial">$132</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$66</td>
+                        <td class="yours">$66</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Rushing Yards</td>
+                        <td class="actual">156</td>
+                        <td class="standard">$78</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$156</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$78</td>
+                        <td class="yours">$78</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Defensive Points Allowed</td>
+                        <td class="actual">17</td>
+                        <td class="standard">$25</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$50</td>
+                        <td class="justwin hidden">$25</td>
+                        <td class="yours">$25</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Passing TDs</td>
+                        <td class="actual">5</td>
+                        <td class="standard">$100</td>
+                        <td class="aerial">$100</td>
+                        <td class="ground">$100</td>
+                        <td class="stingyd">$100</td>
+                        <td class="justwin hidden">$100</td>
+                        <td class="yours">$100</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Rushing TDs</td>
+                        <td class="actual">1</td>
+                        <td class="standard">$30</td>
+                        <td class="aerial">$30</td>
+                        <td class="ground">$30</td>
+                        <td class="stingyd">$30</td>
+                        <td class="justwin hidden">$30</td>
+                        <td class="yours">$30</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Defensive / Special Team TDs</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Extra Points</td>
+                        <td class="actual">6</td>
+                        <td class="standard">$30</td>
+                        <td class="aerial">$30</td>
+                        <td class="ground">$30</td>
+                        <td class="stingyd">$30</td>
+                        <td class="justwin hidden">$30</td>
+                        <td class="yours">$30</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Field Goals: 0-39</td>
+                        <td class="actual">1</td>
+                        <td class="standard">$15</td>
+                        <td class="aerial">$15</td>
+                        <td class="ground">$15</td>
+                        <td class="stingyd">$15</td>
+                        <td class="justwin hidden">$15</td>
+                        <td class="yours">$15</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Field Goals: 40-49</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Field Goals: 50+</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Sacks</td>
+                        <td class="actual">3</td>
+                        <td class="standard">$15</td>
+                        <td class="aerial">$15</td>
+                        <td class="ground">$15</td>
+                        <td class="stingyd">$15</td>
+                        <td class="justwin hidden">$15</td>
+                        <td class="yours">$15</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Interceptions</td>
+                        <td class="actual">2</td>
+                        <td class="standard">$20</td>
+                        <td class="aerial">$20</td>
+                        <td class="ground">$20</td>
+                        <td class="stingyd">$20</td>
+                        <td class="justwin hidden">$20</td>
+                        <td class="yours">$20</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">Safeties</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Blocked Kicks</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="odd">
+                        <td class="stat label">2pt Conversions</td>
+                        <td class="actual">0</td>
+                        <td class="standard">$0</td>
+                        <td class="aerial">$0</td>
+                        <td class="ground">$0</td>
+                        <td class="stingyd">$0</td>
+                        <td class="justwin hidden">$0</td>
+                        <td class="yours">$0</td>
+                    </tr>
+                    <tr class="even">
+                        <td class="stat label">Team Win</td>
+                        <td class="actual">Yes</td>
+                        <td class="standard">$68</td>
+                        <td class="aerial">$68</td>
+                        <td class="ground">$68</td>
+                        <td class="stingyd">$68</td>
+                        <td class="justwin hidden">$136</td>
+                        <td class="yours">$136</td>
+                    </tr>
+                    <tr class="odd total-income">
+                        <th class="stat label">Total Income</th>
+                        <th class="actual"></th>
+                        <th class="standard">$457</th>
+                        <th class="aerial">$420</th>
+                        <th class="ground">$444</th>
+                        <th class="stingyd">$338</th>
+                        <th class="justwin hidden">$525</th>
+                        <th class="yours">$525</th>
+                    </tr>
+                </table>
+            </div>
+            <!-- TODO: add a 'compare with other players' table -->
+        </form>
+    </div>
+</body>
+</html>
